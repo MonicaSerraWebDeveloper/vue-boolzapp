4c448814db189bdce62e74d32a9148c701fb9148
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-05 20:38:50 +0200

    feat: add the comment and fix the indentation

--- js/script.js
+++ js/script.js
@@ -101,6 +101,7 @@ createApp({
         };
     },
     methods: {
+        // Creiamo una variabile dentro la funzione per legare insieme tutti gli elementi che andranno a dare l'immagine dinamiche ai contatti nell'aside degli Address Book
         imageSource: function(dynamicImage) {
             const pathImage = './img/avatar'
             const extensionImage = '.jpg'
@@ -108,10 +109,12 @@ createApp({
             return imageSourceCompleted
         },
 
+        // Creiamo la funzione per selezionare correttamente l'utente che viene cliccato nell'aside dell'address book
         userSelected: function(indexContacts) {
           this.truthIndex = indexContacts
         },
 
+        // Creiamo una variabile che ci permette di pushare un oggetto che contiene la data e l'ora in cui è stato inviato il messaggio, il messaggio e lo status del messaggio, mettiamo tutto dentro una variabile e pushiamo l'oggetto dentro l'array object di "messages"
         messageChat: function(indexContacts) {
           const newMessage = {
             date: dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT) + ' ' + dt.now().setLocale('it').toLocaleString(dt.TIME_24_WITH_SECONDS),
@@ -121,19 +124,20 @@ createApp({
           this.contacts[indexContacts].messages.push(newMessage);
           this.chatMessageInput = '';
 
-          // Aggiungiamo il TimeOut per far partire un messaggio in automatico e lo pushiamo all'interno dell'array "messsages" che si trova all'interno dell'array di oggetti di "contacts"
-          const automatedMessage = {
-            date: dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT) + ' ' + dt.now().plus({ seconds: 2 }).setLocale('it').toLocaleString(dt.TIME_24_WITH_SECONDS),
-            message: 'ok',
-            status: 'received'
-          }
+        // Aggiungiamo il TimeOut per far partire un messaggio in automatico e lo pushiamo all'interno dell'array "messsages" che si trova all'interno dell'array di oggetti di "contacts"
+        const automatedMessage = {
+          date: dt.now().setLocale('it').toLocaleString(dt.DATE_SHORT) + ' ' + dt.now().plus({ seconds: 2 }).setLocale('it').toLocaleString(dt.TIME_24_WITH_SECONDS),
+          message: 'ok',
+          status: 'received'
+        }
 
-          this.okMessage = setTimeout(() => {
-            this.contacts[indexContacts].messages.push(automatedMessage)
-          }, 2000);
-          
+        this.okMessage = setTimeout(() => {
+          this.contacts[indexContacts].messages.push(automatedMessage)
+        }, 2000);
+        
         },
 
+        // Creiamo una funzione che ci permette di cercare gli utenti nella barra di ricerca dei nostri contatti nell'aside di Address Book, con un ciclo forEach andiamo a ciclare il nostro array object per prendere di volta in volta la chiave "name" che ci occorre per confrontare la stringa del nome a ciò che viene scritto nel campo input della ricerca utente. Dentro il ciclo for stabiliamo una variabile in cui richiamiamo la variabile vuota che è collegata al v-model con includes verifichiamo che ciò che viene scritto sia uguale e se la chiave dentro contacts "corrisponde" a true allora la condizione è verificata altrimenti no e il contatto sarà visibile nell'aside di Address Book oppure no. per riuscire a filtrare correttamente la funzione è stata richiamata nel campo input tramite l'evento @input
         searchUser: function() {
           this.contacts.forEach(contact => {
             const inputLowerCase = this.inputToSearchUser.toLowerCase()
@@ -156,6 +160,7 @@ createApp({
           this.showIconChevron = false
         },
 
+        // Creiamo una funzione che ci permette di mostrare al click una finestra con delle opzioni all'interno nei messaggi della chat, per farlo abbiamo stabilito prima una variabile "showDropdown" che ha un valore booleano e il click funziona solo quando questa variabile è il contrario di se stessa, in questo modo abbiamo creato una sorta di toggle, definiamo anche due argomenti per richiamare correttamente la finestra per il messaggio che clicchiamo
         clickDropdown: function(indexContact, indexMessage) {
           this.truthIndex = indexContact
           this.truthMessageIndex = indexMessage
@@ -163,6 +168,7 @@ createApp({
           this.showDropdown = !this.showDropdown
         },
 
+        // Creiamo una funzione che ci permette di chiudere il dropdown opzionale del singolo messaggio ogni volta che viene fatto un click ovunque tranne che sull'icona dello chevron. Se si clicca sull'icona allora il clickDown si attiva se si clicca altrove se il clickDown è attivo questo si chiude se non attivo rimane chiuso
         closeDropdown: function($event) {
 
           if (!$event.target.classList.contains('fa-chevron-down')) {
@@ -172,12 +178,11 @@ createApp({
           }
         },
 
+        // Creiamo una funzione che ci permette di eliminare un oggetto all'interno dell'array "messages" una volta che viene cliccata l'opzione all'interno della finestra dei dropdown "delete"
         deleteMessage: function(indexMessage) {
           this.contacts[this.truthIndex].messages.splice(indexMessage, 1)
         }
     },
-    mounted() {
-      this.searchUser()
-    }
+
   
 }).mount('#app');
